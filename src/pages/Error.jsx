b3ccbdf5d501c788e608457e5a3700eb63c3bc02
Rameez Ahmad <rameezahmad20131a4245@gmail.com>
2023-07-13 20:24:15 +0530

    changed error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import error from "../assets/Images/page-not-found.svg"
-import "./Error.css";
 
 const Error = () => {
     return (
@@ -15,7 +14,7 @@ const Error = () => {
                 </div>
                 <div className="flex flex-col items-center">
                     <h1 className="text-2xl font-semibold">404 Page Not Found</h1>
-                    <a className="px-10 text-xl hover:scale-95 transition-all duration-200 py-2 rounded-md font-semibold my-6 text-richblack-300 border border-richblack-400 hover:bg-yellow-25 hover:text-richblack-900" href="/">
+                    <a className="px-10 text-xl hover:scale-95 transition-all duration-200 py-2 rounded-md font-semibold my-6 text-richblack-300 border border-richblack-400 hover:bg-yellow-50 hover:text-richblack-900" href="/">
                         Back to home
                     </a>
                 </div>
